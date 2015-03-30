69a09658da5bfbbdad0fefeb4aff9951db8c9c77
Grant Timmerman <granttimmerman@gmail.com>
2015-03-29 23:34:04 -0700

    Add event schedule and faq

--- src/components/faq_section/faq_section.jsx
+++ src/components/faq_section/faq_section.jsx
@@ -4,6 +4,29 @@ module.exports = React.createClass({
   render: function() {
     return (
       <section className='FAQSection page-section'>
+        <h2 className='title'>FAQ</h2>
+        <ul className='faq'>
+          <li className='qa'>
+            <h4 className='question'>Who can attend?</h4>
+            <p>Current undergraduate university students and high school students</p>
+          </li>
+          <li className='qa'>
+            <h4 className='question'>Travel Reimbursement?</h4>
+            <p>We are <strong>not</strong> providing travel reimbursement.</p>
+          </li>
+          <li className='qa'>
+            <h4 className='question'>What Should I bring?</h4>
+            <p>Please bring your student id, laptop, charger and passion! Besides that, a toothbrush, sleeping bag/blanket, and change of clothes is recommended.</p>
+          </li>
+          <li className='qa'>
+            <h4 className='question'>How Big are Teams?</h4>
+            <p>A team can be as big as 5 people. We'll help with pairing team members if you don't come with one.</p>
+          </li>
+          <li className='qa'>
+            <h4 className='question'>I have a different question. Who can I ask?</h4>
+            <p>Reach out to us at via <a href='twitter.com/dubhacks'>Twitter</a>, <a href='facebook.com/uwhacks'>Facebook</a>, or send an email to <a href='contact@dubhacks.co'>contact@dubhacks.co</a></p>
+          </li>
+        </ul>
       </section>
     );
   }
--- src/components/schedule_section/schedule_section.jsx
+++ src/components/schedule_section/schedule_section.jsx
@@ -4,6 +4,17 @@ module.exports = React.createClass({
   render: function() {
     return (
       <section className='ScheduleSection page-section'>
+        <h2 className='title'>Event Schedule</h2>
+        <ul className='days'>
+          <li className='day'>
+            <h3 className='day-title'>Saturday, May 16, 2015</h3>
+            <ul className='times'></ul>
+          </li>
+          <li className='day'>
+            <h3 className='day-title'>Sunday, May 17, 2015</h3>
+            <ul className='times'></ul>
+          </li>
+        </ul>
       </section>
     );
   }
