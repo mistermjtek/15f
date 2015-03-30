43b9745ca1aaa98c857a1310024ac6a7e41b3022
Grant Timmerman <granttimmerman@gmail.com>
2015-03-30 00:00:25 -0700

    Add description

--- src/components/description_section/description_section.jsx
+++ src/components/description_section/description_section.jsx
@@ -4,6 +4,13 @@ module.exports = React.createClass({
   render: function() {
     return (
       <section className='DescriptionSection page-section'>
+        <div className='col'>
+        </div>
+        <div className='col'>
+          <h2 className='title'>Educate Yourself</h2>
+          <p className='description'> This Spring, 200 college hackers will gather at
+          the HUB to build the next generation of innovative software and hardware hacks.</p>
+        </div>
       </section>
     );
   }
