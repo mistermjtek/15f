b2b241c999b77de436521f629690e3408aa8ef1a
Grant Timmerman <granttimmerman@gmail.com>
2015-04-02 00:12:16 -0700

    Add footer

--- src/components/footer_section/footer_section.jsx
+++ src/components/footer_section/footer_section.jsx
@@ -4,32 +4,38 @@ module.exports = React.createClass({
   render: function() {
     return (
       <section className='FooterSection page-section'>
-        <div className='col organizers'>
-          <h5 className='title'>Spring 2015 DubHacks Organizers</h5>
-          <ul className='organizers'>
-            <li>Grant Timmerman</li>
-            <li>Skyler Kidd</li>
-            <li>Mahir Kothary</li>
-            <li>Naomi Musgrave</li>
-            <li>Navid Azodi</li>
-            <li>David Coven</li>
-            <li>Karan Goel</li>
-            <li>Anwell Wang</li>
-            <li>Christopher Su</li>
-            <li>Lucas Colusso</li>
-            <li>Malia Imayama</li>
-          </ul>
+        <div className='main footer'>
+          <div className='col organizers'>
+            <h5 className='title'>Spring 2015 DubHacks Organizers</h5>
+            <ul className='organizers-list'>
+              <li>Grant Timmerman</li>
+              <li>Skyler Kidd</li>
+              <li>Mahir Kothary</li>
+              <li>Naomi Musgrave</li>
+              <li>Navid Azodi</li>
+              <li>David Coven</li>
+              <li>Karan Goel</li>
+              <li>Anwell Wang</li>
+              <li>Christopher Su</li>
+              <li>Lucas Colusso</li>
+              <li>Malia Imayama</li>
+            </ul>
+          </div>
+          <div className='col logo'>
+            <img className='logo' src='/images/logo.svg' />
+          </div>
+          <div className='col links'>
+            <h5 className='title'>Links</h5>
+            <ul className='links-list'>
+              <li><a href='http://hackcodeofconduct.org/dubhacks_f14'>Code of Conduct</a></li>
+              <li><a href='contact@dubhacks.co'>Contact Us</a></li>
+              <li><a href='dubhacks.pr.co'>Press</a></li>
+            </ul>
+          </div>
         </div>
-        <div className='col'>
-          <h5 className='links'>Links</h5>
-          <ul>
-            <li><a href='http://hackcodeofconduct.org/dubhacks_f14'>Code of Conduct</a></li>
-            <li><a href='dubhacks.pr.co'>Press</a></li>
-            <li><a href='contact@dubhacks.co'>Contact Us</a></li>
-          </ul>
-        </div>
-        <div className='col'>
-          <span>Made with ❤ in Seattle</span>
+        <div className='sub footer'>
+          <div className='love'>Made with <span className='red'>❤</span> in Seattle</div>
+          <p className='TOS'>DubHacks is a student event. Our sponsors are sponsors of the event and not of the University of Washington.<br/>There is no implied endorsement of these companies by the University.</p>
         </div>
       </section>
     );
