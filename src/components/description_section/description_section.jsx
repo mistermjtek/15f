b44535bff3408440a4767780ba95383640ff7ef2
Grant Timmerman <granttimmerman@gmail.com>
2015-04-05 00:12:11 -0700

    Made header responsive

--- src/components/description_section/description_section.jsx
+++ src/components/description_section/description_section.jsx
@@ -7,7 +7,7 @@ module.exports = React.createClass({
         <div className='text'>
           <h2 className='section title'>Educate Yourself</h2>
           <p className='description'> This Spring, 200 college hackers will gather at
-          Mary Gates Hall to build<br/> the next generation of innovative software and hardware hacks.</p>
+          Mary Gates Hall to build the next generation of innovative software and hardware hacks.</p>
         </div>
         <img className='trees' src='images/trees.svg' />
       </section>
