e852f2c689e4f6c0620c0695d9e102f217ca5e3b
Grant Timmerman <granttimmerman@gmail.com>
2015-04-05 22:44:31 -0700

    Update faq format

--- src/components/faq_section/faq_section.jsx
+++ src/components/faq_section/faq_section.jsx
@@ -5,48 +5,52 @@ module.exports = React.createClass({
     return (
       <section className='FAQSection page-section' id='faq'>
         <h2 className='section title'>FAQ</h2>
-        <ul className='faq'>
-          <li className='qa'>
-            <h4 className='question'>What is DubHacks?</h4>
-            <p>DubHacks is the largest student-run college hackathon in the Pacific Northwest. The first of its kind in the Pacific Northwest, student developers and designers gather at the University of Washington in Seattle campus to form teams and build projects with the goal of creating solutions to real-world problems and learning. This is the second-ever DubHacks event.</p>
-          </li>
-          <li className='qa'>
-            <h4 className='question'>Who can attend?</h4>
-            <p>Undergraduate university students of all backgrounds are encouraged to apply and attend DubHacks.</p>
-          </li>
-          <li className='qa'>
-            <h4 className='question'>Travel Reimbursement?</h4>
-            <p>We are <strong>not</strong> providing travel reimbursement.</p>
-          </li>
-          <li className='qa'>
-            <h4 className='question'>How much does this event cost?</h4>
-            <p>Absolutely free. We will provide you WiFi, meals, caffeine, swag, and the workspace.</p>
-          </li>
-          <li className='qa'>
-            <h4 className='question'>What Should I bring?</h4>
-            <p>Please bring a laptop, charger, student id and passion! Besides that, a toothbrush, sleeping bag/blanket, and change of clothes is recommended.</p>
-          </li>
-          <li className='qa'>
-            <h4 className='question'>What should I build?</h4>
-            <p>Anything really. The project is up to you and your team. You have 24 hours to make anything, ranging from web apps, desktop apps, mobile apps, or even traditional software.</p>
-          </li>
-          <li className='qa'>
-            <h4 className='question'>How big are Teams?</h4>
-            <p>A team can be as big as 5 people. We'll help with pairing team members if you don't come with one.</p>
-          </li>
-          <li className='qa'>
-            <h4 className='question'>Will you have hardware prototyping equipment?</h4>
-            <p>Yes. Our partners at MLH will have limited stock of Oculus Rifts, Myo arm-bands, Leap Motions, Arduinos, 3D printers and more to give out to hackers for the duration of the hackathon.</p>
-          </li>
-          <li className='qa'>
-            <h4 className='question'>Are we allowed to build on past projects?</h4>
-            <p>A hackathon is an experience to build on something completely new within a set amount of time. With that in mind, you cannot work on past projects. Using your own APIs or third-party APIs is alright as long as the project is a fresh start.</p>
-          </li>
-          <li className='qa'>
-            <h4 className='question'>I have a different question. Who can I ask?</h4>
-            <p>Reach out to us at via <a href='twitter.com/dubhacks'>Twitter</a>, <a href='facebook.com/uwhacks'>Facebook</a>, or send an email to <a href='contact@dubhacks.co'>contact@dubhacks.co</a></p>
-          </li>
-        </ul>
+        <div className='faqs'>
+          <ul className='faq'>
+            <li className='qa'>
+              <h4 className='question'>What is DubHacks?</h4>
+              <p>DubHacks is the largest student-run college hackathon in the Pacific Northwest. The first of its kind in the Pacific Northwest, student developers and designers gather at the University of Washington in Seattle campus to form teams and build projects with the goal of creating solutions to real-world problems and learning. This is the second-ever DubHacks event.</p>
+            </li>
+            <li className='qa'>
+              <h4 className='question'>Who can attend?</h4>
+              <p>Undergraduate university students of all backgrounds are encouraged to apply and attend DubHacks.</p>
+            </li>
+            <li className='qa'>
+              <h4 className='question'>Travel reimbursement?</h4>
+              <p>We are <strong>not</strong> providing travel reimbursement.</p>
+            </li>
+            <li className='qa'>
+              <h4 className='question'>How much does this event cost?</h4>
+              <p>Absolutely free. We will provide you WiFi, meals, caffeine, swag, and the workspace.</p>
+            </li>
+            <li className='qa'>
+              <h4 className='question'>What should I bring?</h4>
+              <p>Please bring a laptop, charger, student id and passion! Besides that, a toothbrush, sleeping bag/blanket, and change of clothes is recommended.</p>
+            </li>
+          </ul>
+          <ul className='faq'>
+            <li className='qa'>
+              <h4 className='question'>What should I build?</h4>
+              <p>Anything really. The project is up to you and your team. You have 24 hours to make anything, ranging from web apps, desktop apps, mobile apps, or even traditional software.</p>
+            </li>
+            <li className='qa'>
+              <h4 className='question'>How big are teams?</h4>
+              <p>A team can be as big as 5 people. We'll help with pairing team members if you don't come with one.</p>
+            </li>
+            <li className='qa'>
+              <h4 className='question'>Will you have hardware prototyping equipment?</h4>
+              <p>Yes. Our partners at MLH will have limited stock of Oculus Rifts, Myo arm-bands, Leap Motions, Arduinos, 3D printers and more to give out to hackers for the duration of the hackathon.</p>
+            </li>
+            <li className='qa'>
+              <h4 className='question'>Are we allowed to build on past projects?</h4>
+              <p>A hackathon is an experience to build on something completely new within a set amount of time. With that in mind, you cannot work on past projects. Using your own APIs or third-party APIs is alright as long as the project is a fresh start.</p>
+            </li>
+            <li className='qa'>
+              <h4 className='question'>I have a different question. Who can I ask?</h4>
+              <p>Reach out to us at via <a href='twitter.com/dubhacks'>Twitter</a>, <a href='facebook.com/uwhacks'>Facebook</a>, or send an email to <a href='contact@dubhacks.co'>contact@dubhacks.co</a></p>
+            </li>
+          </ul>
+        </div>
       </section>
     );
   }
