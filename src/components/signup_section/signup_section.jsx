c1493d3b405fd9ad1c7316cb813719001472ff23
Grant Timmerman <granttimmerman@gmail.com>
2015-08-08 17:25:43 -0700

    Update signup section formatting and styling

--- src/components/signup_section/signup_section.jsx
+++ src/components/signup_section/signup_section.jsx
@@ -11,12 +11,18 @@ var MailChimpStyles = {
   };
 
 module.exports = React.createClass({
-getInitialState: function() {
-    return {value: ''};
+  getInitialState: function() {
+    return {
+      value: ''
+    };
   },
+
   handleChange: function(event) {
-    this.setState({value: event.target.value});
+    this.setState({
+      value: event.target.value
+    });
   },
+
   render: function() {
     var value = this.state.value;
     return (
@@ -24,23 +30,27 @@ getInitialState: function() {
         <div id="signup">
           <h2 className='section title'>SIGN UP FOR OUR MAILING LIST</h2>
           <div id="mc_embed_signup">
-          <form action="//dubhacks.us9.list-manage.com/subscribe/post?u=18be1407915b0713dddc4da62&amp;id=1813ccd943" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate" target="_blank" formNoValidate>
-          <div id="mc_embed_signup_scroll">
-            <label>Enter your email to be updated for our event this fall</label>
-          <div className="mc-field-group">
-          <input onChange={this.handleChange} value={value} type="email" name="EMAIL" className="required email" id="mce-EMAIL" placeholder={"Enter email here"}  />
-          </div>
-          <div id="mce-responses" className="clear">
-          <div className="response" id="mce-error-response" style={MailChimpStyles.hide}></div>
-          <div className="response" id="mce-success-response" style={MailChimpStyles.hide}></div>
-          </div>
-          <div style={MailChimpStyles.etc}><input type="text" name="b_18be1407915b0713dddc4da62_1813ccd943" tabindex="-1" value="" /></div>
-          <div className="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" className="button" /></div>
-          </div>
-          </form>
+            <form action="//dubhacks.us9.list-manage.com/subscribe/post?u=18be1407915b0713dddc4da62&amp;id=1813ccd943" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate" target="_blank" formNoValidate>
+              <div id="mc_embed_signup_scroll">
+                <label>Enter your email to be updated for our event this fall</label>
+                <div className="mc-field-group">
+                  <input onChange={this.handleChange} value={value} type="email" name="EMAIL" className="required email" id="mce-EMAIL" placeholder={"Enter email here"}  />
+                </div>
+                <div id="mce-responses" className="clear">
+                  <div className="response" id="mce-error-response" style={MailChimpStyles.hide}></div>
+                  <div className="response" id="mce-success-response" style={MailChimpStyles.hide}></div>
+                </div>
+                <div style={MailChimpStyles.etc}>
+                  <input type="text" name="b_18be1407915b0713dddc4da62_1813ccd943" tabindex="-1" value="" />
+                </div>
+                <div className="clear">
+                  <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" className="button" />
+                </div>
+              </div>
+            </form>
           </div>
         </div>
-        </section>
-      );
-    }
+      </section>
+    );
+  }
 });
