23efb35ef0986a931d7ac79bbddcc7fea14248b7
Michael Jeffrey Wu <michaeljeffreywu@gmail.com>
2016-05-21 02:13:15 -0700

    modify description

--- src/components/description_section/description_section.jsx
+++ src/components/description_section/description_section.jsx
@@ -6,7 +6,7 @@ module.exports = React.createClass({
       <section className='DescriptionSection page-section'>
         <div className='text'>
           <h2 className='section title'>Who Am I?</h2>
-          <p className='description'>Recent <strong>University of California, San Diego</strong> graduate working as a web developer for 2 years. I am an allround web developer. I am a senior programmer with good knowledge of front-end techniques. I love structure and order and I also stand for quality. I love spending time on fixing little details and optimizing web apps. Also I like working in a team, you'll learn faster and more. As the saying goes: 'two heads are better than one'.</p>
+          <p className='description'>Recent <strong>University of California, San Diego</strong> graduate working as a web developer for 2 years. I am a front end developer focused on building beautiful and optimized applications. I love structure and order so I stand for elegant code. I love spending time on fixing little details and optimizing web apps. My passion is learning and using the most recent application frameworks to create scalable products for small companies looking to grow or large companies looking for better user experiences. I believe great user interfaces that understand human needs are the driving force of today’s successful businesses.</p>
         </div>
       </section>
     );
